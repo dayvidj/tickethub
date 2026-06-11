2d78ce7b4a515dd1bd1f47383b01695f103dcbff
Dayvid Jonathan <dayvidaf7@gmail.com>
2026-06-11 14:46:57 -0300

    Add Cliente entity for customer representation in the system

---
+++ src/main/java/com/dayvid/ticketeira/entity/Cliente.java
@@ -0,0 +1,21 @@
+package com.dayvid.ticketeira.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+}
