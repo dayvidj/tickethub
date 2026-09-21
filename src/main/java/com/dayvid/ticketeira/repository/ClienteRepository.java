8bb71d02e45cec6fda7de0b39c09ba7b9a71923b
Dayvid Jonathan <dayvidaf7@gmail.com>
2026-09-21 15:26:21 -0300

    Add existence check methods in ClienteRepository for email and CPF

--- src/main/java/com/dayvid/ticketeira/repository/ClienteRepository.java
+++ src/main/java/com/dayvid/ticketeira/repository/ClienteRepository.java
@@ -4,4 +4,6 @@ import com.dayvid.ticketeira.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
 }
